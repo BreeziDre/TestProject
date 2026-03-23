da0e81f186a5d72b9ef7a9de3dad8cfa35565374
pc <pc@localhost>
2026-03-22 19:58:32 -0500

    Started my side of the work

---
+++ testproject/src/testproject/TreeNode.java
@@ -0,0 +1,36 @@
+package testproject;
+
+public class TreeNode 
+{
+	Product data;
+	TreeNode left;
+	TreeNode right;
+	
+
+	public TreeNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(TreeNode left) {
+		this.left = left;
+	}
+
+	public TreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(TreeNode right) {
+		this.right = right;
+	}
+
+	public void setData(Product data) {
+		this.data = data;
+	}
+
+	public Product getData() {
+		return data;
+	}
+	
+	
+	
+}
